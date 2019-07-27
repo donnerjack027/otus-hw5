02a21b6eb30b6141286248d119129d78cffcca25
vasiliev_va <vasiliev_va@protei.ru>
2019-07-27 13:56:46 +0300

    jenkins 2

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,11 +1,25 @@
 pipeline {
-    agent { dockerfile true }
-    stages {
-        stage('Test') {
-            steps {
-                sh 'git --version'
-                sh 'pytest --version'
+    agent none
+    stage ('Checkout') {
+        agent any
+        steps {
+            git(
+                url: 'https://github.com/donnerjack027/otus-hw5.git',
+                credentialsId: 'CREDENTIALS',
+                branch: "master"
+            )
+        }
+    }
+    stage ('Build') {
+        agent {
+            dockerfile {
+                filename 'Dockerfile'
+                args '--network=host'
             }
+        }
+        steps {
+            sh 'git --version'
+            sh 'pytest --version'
         }
     }
 }
