edc2dcfeff2fb0928eb9e694fab03bf9bb02e3bb
vasiliev_va <vasiliev_va@protei.ru>
2019-07-27 14:02:47 +0300

    fix

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,27 +1,12 @@
 pipeline {
-    stages {
-        agent none
-        stage ('Checkout') {
-            agent any
-            steps {
-                git(
-                    url: 'https://github.com/donnerjack027/otus-hw5.git',
-                    credentialsId: 'CREDENTIALS',
-                    branch: "master"
-                )
-            }
+    agent {
+        dockerfile {
+            filename 'Dockerfile'
+            args '--network=host'
         }
-        stage ('Build') {
-            agent {
-                dockerfile {
-                    filename 'Dockerfile'
-                    args '--network=host'
-                }
-            }
-            steps {
-                sh 'git --version'
-                sh 'pytest --version'
-            }
+        steps {
+            sh 'git --version'
+            sh 'pytest --version'
         }
     }
 }
