da8e35fb1f189fed89562b1115ed323ff6c01065
vasiliev_va <vasiliev_va@protei.ru>
2019-07-27 13:17:00 +0300

    homework 20-21

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,11 @@
+pipeline {
+    agent { dockerfile true }
+    stages {
+        stage('Test') {
+            steps {
+                sh 'git --version'
+                sh 'pytest --version'
+            }
+        }
+    }
+}
