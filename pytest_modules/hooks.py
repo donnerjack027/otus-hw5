376661297978d470e5ec906452eef7db2bae2b76
donnerjack027 <donnerdjek027@gmail.com>
2019-07-28 19:39:58 +0300

    temp fix

--- pytest_modules/hooks.py
+++ pytest_modules/hooks.py
@@ -1,37 +1,37 @@
-"""
-pytest hooks
-"""
-import pytest
-
-
-def pytest_addoption(parser):
-    """
-    addoption
-    """
-    parser.addoption("--browser", action="store", default="firefox",
-                     help="Enter browser name. Supported:'firefox', 'chrome'")
-    parser.addoption("--address", action="store",
-                     default="http://192.168.110.60/opencart/admin/index.php?route=common/login",
-                     help="Enter opencart url")
-    parser.addoption("--timeout", action="store", default="3",
-                     help="Enter page load timeout")
-
-
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    outcome = yield
-    report = outcome.get_result()
-    setattr(item, "rep_" + report.when, report)
-    if report.when == 'call':
-        report.test_metadata = 'whatever'
-        report.stage_metadata = {
-            'Test status': 'start of run'
-        }
-    elif report.when == 'setup':
-        report.stage_metadata = {
-            'Test status': 'in progress'
-        }
-    elif report.when == 'teardown':
-        report.stage_metadata = {
-            'Test status': 'test finished'
-        }
+# """
+# pytest hooks
+# """
+# import pytest
+#
+#
+# def pytest_addoption(parser):
+#     """
+#     addoption
+#     """
+#     parser.addoption("--browser", action="store", default="firefox",
+#                      help="Enter browser name. Supported:'firefox', 'chrome'")
+#     parser.addoption("--address", action="store",
+#                      default="http://192.168.110.60/opencart/admin/index.php?route=common/login",
+#                      help="Enter opencart url")
+#     parser.addoption("--timeout", action="store", default="3",
+#                      help="Enter page load timeout")
+#
+#
+# @pytest.hookimpl(tryfirst=True, hookwrapper=True)
+# def pytest_runtest_makereport(item, call):
+#     outcome = yield
+#     report = outcome.get_result()
+#     setattr(item, "rep_" + report.when, report)
+#     if report.when == 'call':
+#         report.test_metadata = 'whatever'
+#         report.stage_metadata = {
+#             'Test status': 'start of run'
+#         }
+#     elif report.when == 'setup':
+#         report.stage_metadata = {
+#             'Test status': 'in progress'
+#         }
+#     elif report.when == 'teardown':
+#         report.stage_metadata = {
+#             'Test status': 'test finished'
+#         }
