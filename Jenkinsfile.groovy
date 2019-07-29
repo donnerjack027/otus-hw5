049caf8ed0a78f466467a7146c8c20e1d7dac758
vasiliev_va <vasiliev_va@protei.ru>
2019-07-29 11:39:12 +0300

    fix

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,7 +19,7 @@ pipeline {
                     jdk: '',
                     properties: [],
                     reportBuildPolicy: 'ALWAYS',
-                    results: [[path: 'target/allure-results']]
+                    results: [[path: '${WORKSPACE}/allure-results']]
             ])
         }
         success {
