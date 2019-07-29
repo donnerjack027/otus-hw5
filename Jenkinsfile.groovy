a318d3f21fcaacce5637d68e68d4efe816f400d1
vasiliev_va <vasiliev_va@protei.ru>
2019-07-29 13:57:54 +0300

    fix

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,16 +7,36 @@ pipeline {
     }
     stages {
         stage('Pylint') {
-                    steps {
-                        sh 'pylint --disable=W1202 --output-format=parseable --reports=no module > pylint.log || echo "pylint exited with $?")'
-                        sh 'cat render/pylint.log'
-                    }
+                steps {
+                    sh 'pylint test_suites/test_suite_hw_26.py'
+                    archiveArtifacts 'render/pylint.log'
                 }
+            }
         stage('Test') {
             steps {
                 sh 'pytest -s -v test_suites/test_suite_hw_26.py  --alluredir ${WORKSPACE}/allure-results'
                 archiveArtifacts '*.ini'
             }
+        }
+    }
+    post {
+        always {
+            allure([
+                    includeProperties: false,
+                    jdk: '',
+                    properties: [],
+                    reportBuildPolicy: 'ALWAYS',
+                    results: [[path: 'allure-results']]
+            ])
+        }
+        success {
+            echo 'Successfully!'
+        }
+        failure {
+            echo 'Failed!'
+        }
+        unstable {
+            echo 'This will run only if the run was marked as unstable'
         }
     }
 }
