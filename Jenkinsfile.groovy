a8dddae5ea6cc66ba210fe865d95dac426172c93
vasiliev_va <vasiliev_va@protei.ru>
2019-07-29 12:37:06 +0300

    fix

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,19 +8,28 @@ pipeline {
     stages {
         stage('Test') {
             steps {
-                sh 'pytest -s -v test_suites/test_suite_hw_26.py  --alluredir ${WORKSPACE}/target/allure-results'
+                sh 'pytest -s -v test_suites/test_suite_hw_26.py  --alluredir ${WORKSPACE}/allure-results'
             }
         }
-        stage('Reports') {
-            steps {
-                    allure([
-                        includeProperties: false,
-                        jdk: '',
-                        properties: [],
-                        reportBuildPolicy: 'ALWAYS',
-                        results: [[path: 'target/allure-results']]
-                    ])
-            }
+    }
+    post {
+        always {
+            allure([
+                    includeProperties: false,
+                    jdk: '',
+                    properties: [],
+                    reportBuildPolicy: 'ALWAYS',
+                    results: [[path: 'allure-results']]
+            ])
+        }
+        success {
+            echo 'Successfully!'
+        }
+        failure {
+            echo 'Failed!'
+        }
+        unstable {
+            echo 'This will run only if the run was marked as unstable'
         }
     }
 }
