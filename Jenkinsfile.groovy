ffd561ad0ca488707a35730bff8e20e5567d1b5c
vasiliev_va <vasiliev_va@protei.ru>
2019-07-29 13:25:04 +0300

    homework 31

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,6 +11,20 @@ pipeline {
                 sh 'pytest -s -v test_suites/test_suite_hw_26.py  --alluredir ${WORKSPACE}/allure-results'
                 archiveArtifacts '*.ini'
             }
+            steps {
+                sh 'pylint --disable=W1202 --output-format=parseable --reports=no module > pylint.log || echo "pylint exited with $?")'
+                sh 'cat render/pylint.log'
+            }
+            step([
+                    $class                     : 'WarningsPublisher',
+                    parserConfigurations       : [[
+                                                          parserName: 'PYLint',
+                                                          pattern   : 'pylint.log'
+                                                  ]],
+                    unstableTotalAll           : '0',
+                    usePreviousBuildAsReference: true
+            ])
         }
+
     }
 }
