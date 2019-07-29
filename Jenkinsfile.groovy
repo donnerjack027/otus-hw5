1506cc84fbd2d9e01bfc98f7697bf995f67f549f
vasiliev_va <vasiliev_va@protei.ru>
2019-07-29 12:30:09 +0300

    fix

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,16 +9,18 @@ pipeline {
         stage('Test') {
             steps {
                 sh 'pytest -s -v test_suites/test_suite_hw_26.py  --alluredir ${WORKSPACE}/target/allure-results'
-                }
             }
+        }
         stage('Reports') {
-                        allure([
-                            includeProperties: false,
-                            jdk: '',
-                            properties: [],
-                            reportBuildPolicy: 'ALWAYS',
-                            results: [[path: 'target/allure-results']]
-                        ])
-                    }
+            steps {
+                    allure([
+                        includeProperties: false,
+                        jdk: '',
+                        properties: [],
+                        reportBuildPolicy: 'ALWAYS',
+                        results: [[path: 'target/allure-results']]
+                    ])
+            }
+        }
     }
 }
