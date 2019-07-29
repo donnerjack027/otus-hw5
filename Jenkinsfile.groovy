cf9bd61211ab5ea2ccfb861e9e4f479d68c7e6f7
vasiliev_va <vasiliev_va@protei.ru>
2019-07-29 12:58:43 +0300

    fix

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,13 +9,33 @@ pipeline {
         stage('Test') {
             steps {
                 sh 'pytest -s -v test_suites/test_suite_hw_26.py  --alluredir ${WORKSPACE}/allure-results'
-                archiveArtifacts '*.log'
+                archiveArtifacts '*.ini'
             }
         }
         stage('Copy artifacts to Nexus-repo') {
             steps {
                 sh "mvn -DskipITs=true -Dsurefire.skip=true deploy"
             }
+        }
+    }
+    post {
+        always {
+            allure([
+                    includeProperties: false,
+                    jdk: '',
+                    properties: [],
+                    reportBuildPolicy: 'ALWAYS',
+                    results: [[path: 'allure-results']]
+            ])
+        }
+        success {
+            echo 'Successfully!'
+        }
+        failure {
+            echo 'Failed!'
+        }
+        unstable {
+            echo 'This will run only if the run was marked as unstable'
         }
     }
 }
