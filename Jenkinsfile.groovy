b2484e998b4bb9e27525194eec9ef1e30242800f
vasiliev_va <vasiliev_va@protei.ru>
2019-07-29 13:54:30 +0300

    fix

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,17 +6,18 @@ pipeline {
         }
     }
     stages {
+        stage('Pylint') {
+                    steps {
+                        sh 'pylint --disable=W1202 --output-format=parseable --reports=no module > pylint.log || echo "pylint exited with $?")'
+                        sh 'cat render/pylint.log'
+                    }
+                }
         stage('Test') {
             steps {
                 sh 'pytest -s -v test_suites/test_suite_hw_26.py  --alluredir ${WORKSPACE}/allure-results'
                 archiveArtifacts '*.ini'
             }
-            steps {
-                sh 'pylint --disable=W1202 --output-format=parseable --reports=no module > pylint.log || echo "pylint exited with $?")'
-                sh 'cat render/pylint.log'
-            }
         }
-
     }
     post {
         always {
