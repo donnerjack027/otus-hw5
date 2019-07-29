f525e89310c3b44bb892606c1085f4dc1ce1f171
vasiliev_va <vasiliev_va@protei.ru>
2019-07-29 14:05:52 +0300

    homework 29

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,8 +8,8 @@ pipeline {
     stages {
         stage('Pylint') {
                 steps {
-                    sh 'pylint --disable=E1206 test_suites/test_suite_hw_26.py'
-                    archiveArtifacts 'render/pylint.log'
+                    sh 'pylint --disable=E1206 test_suites/test_suite_hw_26.py > pylint.log'
+                    archiveArtifacts 'pylint.log'
                 }
             }
         stage('Test') {
@@ -17,26 +17,6 @@ pipeline {
                 sh 'pytest -s -v test_suites/test_suite_hw_26.py  --alluredir ${WORKSPACE}/allure-results'
                 archiveArtifacts '*.ini'
             }
-        }
-    }
-    post {
-        always {
-            allure([
-                    includeProperties: false,
-                    jdk: '',
-                    properties: [],
-                    reportBuildPolicy: 'ALWAYS',
-                    results: [[path: 'allure-results']]
-            ])
-        }
-        success {
-            echo 'Successfully!'
-        }
-        failure {
-            echo 'Failed!'
-        }
-        unstable {
-            echo 'This will run only if the run was marked as unstable'
         }
     }
 }
